39697747e01b3abdd4f93bc3ad64b990e2230f72
Ibrahim Helal <contact.ihelal@gmail.com>
2024-01-11 15:51:55 -0800

    Init commit

---
+++ select.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+IMAGE=`yq r /index.yaml 'simulators.*.image' | fzf --preview "yq r /index.yaml 'simulators.(image=={})'"`
+echo "Updating docker-compose.yml to use ${IMAGE} simulation container..."
+yq w -i /work/docker-compose.yml services.simulator.image $IMAGE
+echo "done."
+echo "Your simulation environment is ready."
+echo "Please enter 'docker-compose up' to launch your environment."
